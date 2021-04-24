5361b37e4057c1a28b0823a234f0804a61de4d92
@shimura.kosuke.infratop <shimura.kosuke.infratop@gmail.com>
2021-04-24 04:37:27 +0000

    adminのログインの遷移先を指定と管理者登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-   mypage_path
+    case resource
+    when Admin
+      mypage_path          
+    when Customer
+      mypage_path              
+    end
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana, :first_name_kana, :zip_code, :address, :telephone_number])
   end
