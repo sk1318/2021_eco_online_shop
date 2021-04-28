32f56a5b7ee4306a068ad54af44f2a84ed4fd87a
@shimura.kosuke.infratop <shimura.kosuke.infratop@gmail.com>
2021-04-28 07:45:03 +0000

    商品詳細ページ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,12 @@
 class Public::ItemsController < ApplicationController
+    include CommonActions
+    before_action :login_check,only:[:show]
     
     def index
         @items = Item.all
+    end
+    
+    def show
+        @item = Item.find(params[:id])
     end
 end
