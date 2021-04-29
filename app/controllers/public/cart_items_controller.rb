b09545445827329b42e6cc76a370fb58fa132d3d
@shimura.kosuke.infratop <shimura.kosuke.infratop@gmail.com>
2021-04-29 07:23:53 +0000

    同じ商品の個数処理

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,18 +3,23 @@ class Public::CartItemsController < ApplicationController
         cart_items = CartItem.all
         is_exist = false
         id = 0
-        cart_item = cart_items.find_by(item_id: params[:item_id])
-        
-        if cart_item
-            is_exist = true
-            id = params[:item_id]
+        cart_items.each do |cart_item|
+            if cart_item.item_id == params[:cart_item][:item_id].to_i
+                is_exist = true
+                id = cart_item.id
+            end
         end
         
         if is_exist
             cart_item = CartItem.find(id)
-            sum = cart_item.amount.to_i + params[:amount].to_i
+            sum = cart_item.amount.to_i + params[:cart_item][:amount].to_i
+            if sum <=10
             cart_item.update(amount: sum)
             redirect_to cart_items_path
+            else
+                flash[:error]= "同じ商品は10個までしか注文できません。"
+                redirect_to cart_items_path
+            end
         else
             cart_item = current_customer.cart_items.new(cart_item_params)
             cart_item.save
@@ -29,9 +34,17 @@ class Public::CartItemsController < ApplicationController
     end
     
     def update
-        @cart_item = CartItem.find(params[:id])
-        @cart_item.update(cart_item_params)
-        redirect_to cart_items_path
+        cart_item = CartItem.find(params[:id])
+        sum = params[:cart_item][:amount].to_i
+        if sum <=10
+            cart_item.update(cart_item_params)
+            redirect_to cart_items_path
+        else
+            @cart_items = current_customer.cart_items.all
+            flash[:error]= "同じ商品は10個までしか注文できません。"
+            redirect_to cart_items_path
+        end
+       
     end
     private
     
