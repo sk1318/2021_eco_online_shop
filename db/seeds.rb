eca793664e5acd29d123050ae281b84f9c2f03fc
@shimura.kosuke.infratop <shimura.kosuke.infratop@gmail.com>
2021-06-13 00:10:42 +0000

    seedsファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(email: 'sk2322@example.com',   password: 'SiSikosuke')
+Admin.create!(email: 'sk2322@example.com',   password: 'SiSikosuke')
+Customer.create!(email: '3222@example.com',password: 'DmmWebcamp',last_name: "田中",
+first_name: "太郎",last_name_kana: "タナカ",first_name_kana: "タロウ",zip_code: "888-888",address: "東京都あきる野市",telephone_number: "000-000-0000")
 Genre.create!(name: "ケーキ")
 Item.create!(genre_id:1,name: "モンブラン",introduction: "おいしいです",price: 200,image_id: "5d833624814b5a0afdc4ba3dcac5b33a1da8cf118a3ac4dd7393f13e3e1a")
 Item.create!(genre_id:1,name: "ショートケーキ",introduction: "おいしいです",price: 200,image_id: "5d833624814b5a0afdc4ba3dcac5b33a1da8cf118a3ac4dd7393f13e3e1a")
