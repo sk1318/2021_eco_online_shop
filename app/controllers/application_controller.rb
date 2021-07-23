8d41c0edfe9d1381b439aee60378563c2f8440ef
EC2 Default User <ec2-user@ip-172-31-19-142.ec2.internal>
2021-07-23 04:44:47 +0000

    customer_topページ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      admin_top_path        
+      admin_customers_path       
     when Customer
       mypage_path   
     end
