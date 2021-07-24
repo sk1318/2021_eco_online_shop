4e034e7216b8ec0a08c7bacca49613a84cbea1d6
sk1318 <sk13188559@outlook.jp>
2021-07-24 03:51:46 +0000

    refile_修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ef57abd375b6d64bb572d836af0476122442d51e103e526041f8b9778bf2b2d065364c5c043f6baa41aca6f19604a39996a267f33f468f62e7ce707c187fa8d7'
