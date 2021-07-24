ab0842bb82644df8e5fc689b900f5fd4ce3c883f
sk1318 <sk13188559@outlook.jp>
2021-07-24 07:28:55 +0000

    注文番号追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < Public::ApplicationController
     def top
-        @items = Item.order(created_at: :desc).limit(4)
+        @items = Item.order(id: :ASC).limit(4)
     end
     
     def about
